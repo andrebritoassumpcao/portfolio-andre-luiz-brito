35a27e9abfcc86268ecc3b2abcc2bf7cc2af92e0
André Brito <andrebritoassumpcao@outlook.com>
2022-10-17 21:26:37 -0300

    Doing the section projetos, and the footer

--- page/js/script.js
+++ page/js/script.js
@@ -1,12 +1,11 @@
-var button = document.getElementById("btn-seta");
-var img = document.getElementById("img-btn");
-
-button.addEventListener("click", function () {
-  var projeto = document.querySelector(".projetos-descricao");
+const toggleCardExpansion = function (id) {
+  var projeto = document.getElementById(`projetos-descricao-${id}`);
+  console.log(projeto);
   projeto.classList.toggle("active");
+  var img = document.getElementById(`button-img-${id}`);
 
   if (projeto.classList.contains("active")) {
     return img.setAttribute("src", "../../assets/icones/seta-redonda-cima.svg");
   }
   return img.setAttribute("src", "../../assets/icones/seta-redonda-baixo.svg");
-});
+};
